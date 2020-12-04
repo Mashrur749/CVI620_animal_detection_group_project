f59b133c8c3b3626842956ace738b275d87f42b9
Musaddiqur Rahman <mashrur749@gmail.com>
2020-12-04 18:03:16 -0500

    table titles made bold

--- results_display/src/components/ConfusionMatrixTable.js
+++ results_display/src/components/ConfusionMatrixTable.js
@@ -41,29 +41,29 @@ export default function ConfusionMatrixTable(props) {
                         
                             <TableCell></TableCell>
                             <TableCell>
-                                <Typography align={"center"} variant="h6" className={classes.sectionTitle}> {props.tableName} </Typography>
+                                <Typography align={"center"} variant="h6" className={classes.sectionTitle}> <b>{props.tableName}</b> </Typography>
                             </TableCell>
                             <TableCell></TableCell>
                                 
                         </TableRow>
                             
                         <TableRow>
-                            <TableCell>Pred/GroundTruth</TableCell>
-                            <TableCell className={classes.tableCell} align="right">Animal</TableCell>
-                            <TableCell className={classes.tableCell} align="right">No Animal</TableCell>
+                            <TableCell><b>Pred/GroundTruth</b></TableCell>
+                            <TableCell className={classes.tableCell} align="right"><b>Animal</b></TableCell>
+                            <TableCell className={classes.tableCell} align="right"><b>No Animal</b></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         <TableRow>
                             <TableCell component="th" scope="row">
-                                Animal
+                                <b>Animal</b>
                             </TableCell>
                             <TableCell className={classes.tableCell} align="right">{props.TP}</TableCell>
                             <TableCell className={classes.tableCell} align="right">{props.FP}</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell component="th" scope="row">
-                                No Animal
+                                <b>No Animal</b>
                             </TableCell>
                             <TableCell className={classes.tableCell} align="right">{props.FN}</TableCell>
                             <TableCell className={classes.tableCell} align="right">{props.TN}</TableCell>
@@ -82,26 +82,26 @@ export default function ConfusionMatrixTable(props) {
                     <TableRow>
                         
                         <TableCell className={classes.tableCell} align="right"></TableCell>
-                        <TableCell className={classes.tableCell} align="right"> Calculation </TableCell>
-                        <TableCell className={classes.tableCell} align="right">Result</TableCell>
+                        <TableCell className={classes.tableCell} align="right"> <b>Calculation</b> </TableCell>
+                        <TableCell className={classes.tableCell} align="right"><b>Result</b></TableCell>
                     </TableRow>
                     <TableRow>
                         <TableCell component="th" scope="row">
-                            Precision
+                            <b>Precision</b>
                         </TableCell>
                         <TableCell className={classes.tableCell} align="right">{props.TP} / ({props.TP} + {props.FP})</TableCell>
                         <TableCell className={classes.tableCell} align="right">{props.Precision}</TableCell>
                     </TableRow>
                     <TableRow>
                         <TableCell component="th" scope="row">
-                            Recall
+                            <b>Recall</b>
                         </TableCell>
                         <TableCell className={classes.tableCell} align="right">{props.TP} / ({props.TP} + {props.FN})</TableCell>
                         <TableCell className={classes.tableCell} align="right">{props.Recall}</TableCell>
                     </TableRow>
                     <TableRow>
                         <TableCell component="th" scope="row">
-                            F-measure
+                            <b>F-measure</b>
                         </TableCell>
                         <TableCell className={classes.tableCell} align="right"> 2 * (({props.Precision} * {props.Recall}) / ({props.Precision} + {props.Recall})) </TableCell>
                         <TableCell className={classes.tableCell} align="right">{props.fMeasure}</TableCell>
